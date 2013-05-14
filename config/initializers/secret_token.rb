df7e1a28ca13b9c470ef11283b4fab4afe19f574
Ruby Ku <rubyku85@gmail.com>
2013-05-14 11:45:04 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Clubsoda::Application.config.secret_token = '9efda68b4ae38b209fa7013c480fcc1f2bea80f37fde28fea1866446e4a13ecdb0c2cadd6d85ad4afee0674e19bb4d86e905f158ffdc61d38a11fef8a1f02c75'
