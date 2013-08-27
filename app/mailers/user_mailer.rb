3411c8efbf8e5bb7184b530d73908690240ab9d3
Ruby Ku <rubyku85@gmail.com>
2013-08-27 17:36:24 -0500

    added sendgrid config

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,9 +1,9 @@
 class UserMailer < ActionMailer::Base
   default from: "henry@clubsodapro.com"
 
-  def contact_form(user_name, user_email, message)
-    @name     = user_name
-    @email    = user_email
+  def contact_form(name, email, message)
+    @name     = name
+    @email    = email
     @message  = message
     mail(to: 'henryrd@gmail.com', subject: 'Someone left you a message at Club Soda')
   end
